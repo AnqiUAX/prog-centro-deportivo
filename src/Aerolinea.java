bc172158447bbb1d8031828e0869063049b26518
Anqi <anqiwang135@gmail.com>
2026-03-04 20:46:04 +0100

    feet(Aviones,Aerolinea,Main):metodo creado

--- src/Aerolinea.java
+++ src/Aerolinea.java
@@ -24,5 +24,74 @@ public class Aerolinea {
         return avionEncontrado;
     }
 
+    private int buscarPrimerHuecoLibre(){
+        int posicionAviones = -1;
+        boolean seguirBuscar = true;
+
+        for(int i = 0; i < CANT_MAX_AVIONES && seguirBuscar; i++){
+            if (catalogo[i] == null) {
+                posicionAviones = i;
+                seguirBuscar = false;
+            }
+        }
+
+        return posicionAviones;
+    }
+
+    public boolean addAviones(Aviones aviones){
+        boolean avionAnadido = false;
+        int posicion;
+
+        if (findAvion(aviones.getId()) == null ) {
+            posicion = buscarPrimerHuecoLibre();
+            if(posicion >= 0){
+                catalogo[posicion] = aviones;
+                avionAnadido = true;
+            }
+        }
+
+        return avionAnadido;
+    }
+
+    public Aviones deleteAviones(int id){
+        Aviones avionEncontrado = null;
+        boolean seguirBuscar = true;
+
+        for(int i = 0; i < CANT_MAX_AVIONES && seguirBuscar; i++){
+            if(catalogo[i] != null && catalogo[i].getId() == id){
+                avionEncontrado = catalogo[i];
+                catalogo[i] = null;
+                seguirBuscar = false;
+            }
+        }
+
+        return avionEncontrado;
+    }
+
+    public boolean updateNumAviones(int idAvion, int numAviones){
+        boolean numAvionesActualizado = false;
+        Aviones aviones;
+
+        aviones = findAvion(idAvion);
+
+        if(aviones != null){
+            numAvionesActualizado = aviones.updateNumPasajeros(numAviones);
+        }
+        return numAvionesActualizado;
+    }
+
+    public String obtianCatalogo(){
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < CANT_MAX_AVIONES; i++){
+            if(catalogo[i] != null){
+                sb.append(catalogo[i].toString());
+            }
+        }
+        sb.append("\n");
+
+        return sb.toString();
+    }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        Aerolinea aerolinea = new Aerolinea(100);
 
+        Aviones aviones1 = new Aviones(1,"Boeing","787-9",290);
+
+        Aviones aviones2 = new Aviones(2,"AirBus","A320",150);
+
+        System.out.println(aerolinea.findAvion(1));
+
+        System.out.println(aerolinea.addAviones(aviones1));
+        System.out.println(aerolinea.addAviones(aviones2));
+
+        System.out.println(aerolinea.obtianCatalogo());
     }
 }
